49361a032d4a61d9fbfe8b6ab51b2c96094dfbf0
Mark Standen <mark@markstanden.dev>
2022-02-26 09:46:58 +0000

    completed scrabble score

--- scrabble-score/src/main/java/Scrabble.java
+++ scrabble-score/src/main/java/Scrabble.java
@@ -1,11 +1,47 @@
-class Scrabble {
+import java.util.*;
+import java.util.stream.IntStream;
 
-    Scrabble(String word) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
+class Scrabble
+{
+	public final Map<Character, Integer> TILE_VALUES = new HashMap<>();
+	private final char[] word;
+	private int[] tileMultiplier;
 
-    int getScore() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
+	private static <K,V> Map<K, V> addTileValue(Map<K, V> map, List<K> mapKeys, V value) {
+		mapKeys.forEach(key -> map.put(key, value));
+		return map;
+	}
+
+	Scrabble(String word, int[] tileMultiplier)
+	{
+		addTileValue(TILE_VALUES,Arrays.asList('A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'), 1);
+		addTileValue(TILE_VALUES,Arrays.asList('D', 'G'), 2);
+		addTileValue(TILE_VALUES,Arrays.asList('B', 'C', 'M', 'P'), 3);
+		addTileValue(TILE_VALUES,Arrays.asList('F', 'H', 'V', 'W', 'Y'), 4);
+		addTileValue(TILE_VALUES,Arrays.asList('K'), 5);
+		addTileValue(TILE_VALUES,Arrays.asList('J', 'X'), 8);
+		addTileValue(TILE_VALUES,Arrays.asList('Q', 'Z'), 10);
+
+		assert word.length() == tileMultiplier.length;
+		this.word = word.toUpperCase().toCharArray();
+		this.tileMultiplier = tileMultiplier;
+		System.out.println("word = " + Arrays.toString(this.word) + ", tileMultiplier = " + Arrays.toString(this.tileMultiplier));
+	}
+
+
+	Scrabble(String word)
+	{
+		this(word, IntStream.iterate(1, x -> 1)
+		                    .limit(word.length())
+		                    .toArray());
+	}
+
+
+	int getScore()
+	{
+		return IntStream.range(0, word.length)
+		         .map(index -> TILE_VALUES.getOrDefault(word[index], 0) * tileMultiplier[index])
+		         .sum();
+	}
 
 }
